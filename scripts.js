7212731e1b376c05fe4fc7576c55b753e6ad2783
22233416 <49471092+22233416@users.noreply.github.com>
2019-08-24 15:01:39 +0800

    Add files via upload
    - Basic web page layout in HTML and CSS.
    - An example of a goal, with its value and each player's score.
    - A rudimentary 'Game' JavaScript object.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-//TBA
+//This file contains all the scripts necessary to run the functionality of the game.
+
+var Game = {
+    G: [0,0]; //An array holding the number of goals available for each player.
+    //** Functionally dependent on V[]? **
+    D: 3; //The number of remaining turns in a trial.
+    V: [[],[]]; //An array holding another array for each player, which represents the value of each goal to that player.
+    Vtie: 5; //The value gained by each player in the result of a tie for any goal.
+    a: [0.9, 0.9]; //The likelihood of making progess towards a goal (i.e. of the card not disintegrating) for each player.
+    
+    S: [[],[]]; //The progess made by each player towards each goal.
+}
+
