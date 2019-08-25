196cfb693c4bf7f23a93b265110b83a25ffc0115
22233416 <49471092+22233416@users.noreply.github.com>
2019-08-25 21:27:39 +0800

    Add files via upload
    The number of goals is now based on the value of G, which is now determined by the length of V, which can be altered in the JS file for different results. It also shows both players' progress towards these goals, which can be changed in the array S - also in the JS file. Currently the program assumes that all goals are open and have the same value to both players.
    The bug that prevented the scripts in the JS document from interacting with the HTML document has been fixed.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,23 @@
 //This file contains all the scripts necessary to run the functionality of the game.
 
 var Game = {
-    G: [0,0]; //An array holding the number of goals available for each player.
-    //** Functionally dependent on V[]? **
-    D: 3; //The number of remaining turns in a trial.
-    V: [[],[]]; //An array holding another array for each player, which represents the value of each goal to that player.
-    Vtie: 5; //The value gained by each player in the result of a tie for any goal.
-    a: [0.9, 0.9]; //The likelihood of making progess towards a goal (i.e. of the card not disintegrating) for each player.
+    D: 3, //The number of remaining turns in a trial.
+    V: [[10,8,5,3],[10,8,5,3]], //An array holding another array for each player, which represents the value of each goal to that player.
     
-    S: [[],[]]; //The progess made by each player towards each goal.
-}
+    //** G is functionally dependent on V[]? **
+    
+    Vtie: 5, //The value gained by each player in the result of a tie for any goal.
+    a: [0.9, 0.9], //The likelihood of making progess towards a goal (i.e. of the card not disintegrating) for each player.
+    S: [[1,3,5,4],[2,6,0,2]] //The progess made by each player towards each goal.
+};
+
+Game.G = [Game.V[0].length, Game.V[1].length]; //An array holding the number of goals available for each player.
 
 function generateGoals(G)
 {
     html = "";
-    for(i=0; i<G; i++)
+    for(i=0; i<G; i++) //Adds G goals for each player.
     {
-        document.getElementById('goals').innerHTML += '<div class="goal"><div class="left score"><h3>2</h3></div><div class="right score"><h3>3</h3></div><h1>8</h1></div>';
+        document.getElementById('goals').innerHTML += '<div class="goal"><div class="left score"><h3 id="score' + i.toString() + '">'+(Game.S[0][i]).toString() + '</h3></div><div class="right score"><h3>' + (Game.S[1][i]).toString() + '</h3></div><h1>' + Game.V[0][i] + '</h1></div>';
     }
 }
