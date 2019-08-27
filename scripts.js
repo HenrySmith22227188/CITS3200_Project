69530fa2ef6c1acdb958888b2dbff26afde26bcf
22233416 <49471092+22233416@users.noreply.github.com>
2019-08-27 10:07:41 +0800

    Add files via upload
    Player scores can now be incremented up to the value of the goal (by clicking them, for now). The issue is that open goals only change state to completed goals (they're meant to turn yellow) for the last goal - currently working on a fix.

--- scripts.js
+++ scripts.js
@@ -8,16 +8,37 @@ var Game = {
     
     Vtie: 5, //The value gained by each player in the result of a tie for any goal.
     a: [0.9, 0.9], //The likelihood of making progess towards a goal (i.e. of the card not disintegrating) for each player.
-    S: [[1,3,5,4],[2,6,0,2]] //The progess made by each player towards each goal.
+    S: [[4,3,2,1],[2,6,0,2]], //The progess made by each player towards each goal.
+    GoalOpen: [true, true, true, true]
 };
 
 Game.G = [Game.V[0].length, Game.V[1].length]; //An array holding the number of goals available for each player.
 
-function generateGoals(G)
+function generateGoals() //A function to add HTML corresponding to the amount of goals (which is defined in the value for G (in the Game object).
 {
-    html = "";
+    html = '';
+    G = Game.G[0];
     for(i=0; i<G; i++) //Adds G goals for each player.
     {
-        document.getElementById('goals').innerHTML += '<div class="goal"><div class="left score"><h3 id="score' + i.toString() + '">'+(Game.S[0][i]).toString() + '</h3></div><div class="right score"><h3>' + (Game.S[1][i]).toString() + '</h3></div><h1>' + Game.V[0][i] + '</h1></div>';
+        ii = i.toString();
+        html += '<div class="goal" id="goal' + ii + '"><div class="left score" onclick="incrementScore(0,1,' + ii + ')"><h3 id="score' + ii + '">'+(Game.S[0][i]).toString() + '</h3></div><div class="right score"><h3>' + (Game.S[1][i]).toString() + '</h3></div><h1>' + Game.V[0][i] + '</h1></div>';
+        
+        document.getElementById('goals').innerHTML = html;
+        
+        if((Game.S[0][i] >= Game.V[0][i])&&(Game.GoalOpen[i])) //Recognises when a player has reached the value for a goal.
+        {
+            document.getElementById('goal' + ii).className += ' completed';
+            Game.GoalOpen[i] = false;
+            // ** TO DO: make this not generate EVERYTHING whenever anything changes. **
+        }
+    }
+}
+
+function incrementScore(p, n, g) //A function to increment the score of a player at a particular goal. p = player [0 or 1], n = score to increment by [int], g = the goal [int] to add points to.
+{
+    if(Game.GoalOpen[g]) //Only open goals can have cards allocated to them.
+    {
+        Game.S[p][g] += n;
+        generateGoals();
     }
 }
