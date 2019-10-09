fa9a841153eb9ee284b4e67ceefe7d5b3c856872
HenrySmith22227188 <49471409+HenrySmith22227188@users.noreply.github.com>
2019-10-09 11:59:44 +0800

    JATOS System Implemented
    folders created: assets.FOLDER, 
                              css.FOLDER, 
                              javascript.FOLDER
    renamed base.html to game.html
    renamed int.json to int.js
    renamed instruction.html to instructions.html
    renamed scripts.js to game.js
    replaced standard html functions to jatos functions where needed
    renamed x to overlay in game.js
    moved 1.ico to assets
    moved game.js to javascript
    moved overlay function from game.js to help_button.js
    moved css files to css folder

---
+++ card_game/int.js
@@ -0,0 +1,20 @@
+{
+	data = {
+		"goalValue": [
+			[10,8,5,3],
+			[10,8,5,3]
+		],
+		"progress": [
+			[1,0,1,0],
+			[0,1,0,1]
+		],
+		"goalOpen": [
+			[true, true, true, true],
+			[true, true, true, true]
+		],
+		"numberOfCards": 5,
+		"probabilityOfProgress": [1, 1],
+		"showOpponentProgress": false,
+		"opponent": false,
+	}
+}
